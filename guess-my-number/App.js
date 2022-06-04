2fd1f751aa3c2071e31309c491e8b50185e960b6
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-03 22:25:23 -0600

    Added GameScreen to App and connected onPress to Confirm button.

--- guess-my-number/App.js
+++ guess-my-number/App.js
@@ -4,15 +4,26 @@ import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
   const [useNumber, setUseNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUseNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (useNumber) {
+    screen = <GameScreen />;
+  }
 
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
       <ImageBackground source={require('./assets/images/dice_background_RN.png')} resizeMode="cover"
         style={styles.rootScreen} imageStyle={styles.backgroundImage}>
-        <StartGameScreen />
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
--- guess-my-number/screens/GameScreen.js
+++ guess-my-number/screens/GameScreen.js
@@ -1,5 +1,17 @@
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 
-function GameScreen() {};
+function GameScreen() {
+  return (
+    <Text style={styles.woohoo}>Woohoo! We did it!</Text>
+  )
+};
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  woohoo: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+  }
+});
