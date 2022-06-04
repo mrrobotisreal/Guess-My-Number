d1e13914f570a144768f0534632c379dd0e4fd73
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-04 12:29:20 -0600

    Added startNewGameHandler to App component and added another useEffect in GameScreen component to reset min and max boundaries for the new game.

--- guess-my-number/App.js
+++ guess-my-number/App.js
@@ -13,6 +13,7 @@ import Colors from './constants/colors';
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const [fontsLoaded] = useFonts({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -34,6 +35,11 @@ export default function App() {
     setGameIsOver(true);
   }
 
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
   if (userNumber) {
@@ -41,7 +47,7 @@ export default function App() {
   }
 
   if (gameIsOver && userNumber) {
-    screen = <GameOverScreen />
+    screen = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onStartNewGame={startNewGameHandler} />
   }
 
   return (
