83061ac87469c8ab0ac898d6cac4397f40709f2f
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-04 12:00:55 -0600

    Added open-sans-bold to Title and open-sans to InstructionText components.

--- guess-my-number/components/ui/Title.js
+++ guess-my-number/components/ui/Title.js
@@ -1,7 +1,5 @@
 import { Text, StyleSheet } from 'react-native';
 
-import Colors from '../../constants/colors';
-
 function Title({ children }) {
   return (
     <Text style={styles.title}>{children}</Text>
@@ -12,6 +10,7 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
+    fontFamily: 'open-sans-bold',
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
