bd2ce1b2af738bc44dad649a00ee02de2dc0a6e5
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-04 13:43:57 -0600

    Finished Guess My Number game! Everything works as expected!

--- guess-my-number/screens/GameScreen.js
+++ guess-my-number/screens/GameScreen.js
@@ -29,7 +29,7 @@ function GameScreen({userNumber, onGameOver}) {
 
   useEffect(() => {
     if (currentGuess ===  userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
 
@@ -76,8 +76,7 @@ function GameScreen({userNumber, onGameOver}) {
             </View>
           </View>
         </Card>
-        <View>
-          {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
+        <View style={styles.listContainer}>
           <FlatList
             data={guessRounds}
             renderItem={itemData => {
@@ -107,5 +106,9 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
   },
 });
