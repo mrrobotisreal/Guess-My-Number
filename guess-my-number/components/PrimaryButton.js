67aa24501145c744ae58dd14738372a55906d861
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-03 21:33:34 -0600

    Added background color and changed centering of the number picking View.

---
+++ guess-my-number/components/PrimaryButton.js
@@ -0,0 +1,39 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log('Pressed!');
+  };
+
+  return (
+      <View style={styles.buttonOuterContainer}>
+        <Pressable onPress={pressHandler} android_ripple={{color: '#644202'}}
+          style={({ pressed }) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}>
+          <Text style={styles.buttonText}>{children}</Text>
+        </Pressable>
+      </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  }
+});
