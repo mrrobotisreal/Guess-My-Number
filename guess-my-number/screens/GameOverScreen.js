69d2762af1858d645bfdbd32bcf71501d4d88177
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-04 12:10:40 -0600

    Added image styling in GameOverScreen.

--- guess-my-number/screens/GameOverScreen.js
+++ guess-my-number/screens/GameOverScreen.js
@@ -1,14 +1,33 @@
-import { View, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
 
 function GameOverScreen() {
   return (
     <View>
       <Title>GAME OVER SUCKA!</Title>
-      <Image source={require('../assets/images/success.png')} />
+      <View style={styles.imageContainer}>
+        <Image source={require('../assets/images/success.png')} style={styles.image} />
+      </View>
     </View>
   );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    width: 400,
+    height: 400,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
