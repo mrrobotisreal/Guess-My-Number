18a73a8e1153fcf9ef22eb8c269d9ae1a9c80f39
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-04 11:21:15 -0600

    Filled out InstructionText component.

--- guess-my-number/components/ui/InstructionText.js
+++ guess-my-number/components/ui/InstructionText.js
@@ -1,3 +1,9 @@
-function InstructionText() {}
+import { Text } from 'react-native';
+
+function InstructionText({children}) {
+  return (
+    <Text style={styles.instructionText}>{content}</Text>
+  );
+}
 
 export default InstructionText;
