d1fc0b06d5e6c9a3e2b08fff602db61c423cda35
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-04 09:58:08 -0600

    Filled out NumberContainer component.

--- guess-my-number/components/game/NumberContainer.js
+++ guess-my-number/components/game/NumberContainer.js
@@ -1,3 +1,11 @@
-function NumberContainer() {};
+import { Text, View, StyleSheet } from 'react-native';
+
+function NumberContainer({children}) {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  )
+};
 
 export default NumberContainer;
