2881efcbf1af754ff406b8d43afccd804dd1707e
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-04 12:14:05 -0600

    Added styling to GameOverScreen and its image.

--- guess-my-number/screens/GameOverScreen.js
+++ guess-my-number/screens/GameOverScreen.js
@@ -5,7 +5,7 @@ import Colors from '../constants/colors';
 
 function GameOverScreen() {
   return (
-    <View>
+    <View style={styles.rootContainer}>
       <Title>GAME OVER SUCKA!</Title>
       <View style={styles.imageContainer}>
         <Image source={require('../assets/images/success.png')} style={styles.image} />
@@ -17,12 +17,18 @@ function GameOverScreen() {
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   imageContainer: {
-    borderRadius: 200,
+    borderRadius: 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
-    width: 400,
-    height: 400,
+    width: 300,
+    height: 300,
     overflow: 'hidden',
     margin: 36,
   },
