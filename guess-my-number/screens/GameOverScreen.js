6c7e1abd9c785669c7ae4433358f2ba080be0d66
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-04 12:23:03 -0600

    Destructured roundsNumber, userNumber, and onStartNewGame in GameOverScreen from App component.

--- guess-my-number/screens/GameOverScreen.js
+++ guess-my-number/screens/GameOverScreen.js
@@ -1,15 +1,18 @@
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
 import Title from '../components/ui/Title';
+import PrimaryButton from '../components/ui/PrimaryButton';
 import Colors from '../constants/colors';
 
-function GameOverScreen() {
+function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
   return (
     <View style={styles.rootContainer}>
       <Title>GAME OVER SUCKA!</Title>
       <View style={styles.imageContainer}>
         <Image source={require('../assets/images/success.png')} style={styles.image} />
       </View>
+      <Text style={styles.summaryText}>Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{userNumber}</Text>.</Text>
+      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -35,5 +38,15 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  summaryText: {
+    fontFamily: 'open-sans',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500,
   },
 });
