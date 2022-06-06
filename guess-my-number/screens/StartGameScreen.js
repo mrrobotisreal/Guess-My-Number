e673ef476e88831c5754f34717ac2ca4e186473f
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-05 19:02:31 -0600

    Added KeyboardAvoidingView to StartGameScreen, created screen style and set it to flex 1 which solved the rendering problem.

--- guess-my-number/screens/StartGameScreen.js
+++ guess-my-number/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet, Alert, useWindowDimensions } from 'react-native';
+import { TextInput, View, StyleSheet, Alert, useWindowDimensions, KeyboardAvoidingView } from 'react-native';
 
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
@@ -37,25 +37,27 @@ function StartGameScreen({ onPickNumber }) {
   const marginTopDistance = height < 380 ? 30 : 60;
 
   return (
-    <View style={[styles.rootContainer, {marginTop: marginTopDistance}]}>
-      <Title>Guess My Number Sucka!</Title>
-      <Card>
-        <InstructionText>Pick a Number</InstructionText>
-        <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad"
-          autoCapitalize="none"
-          autoCorrect={false}
-          value={enteredNumber}
-          onChangeText={numberInputHandler} />
-        <View style={styles.buttonsContainer}>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+    <KeyboardAvoidingView style={styles.screen}>
+      <View style={[styles.rootContainer, {marginTop: marginTopDistance}]}>
+        <Title>Guess My Number Sucka!</Title>
+        <Card>
+          <InstructionText>Pick a Number</InstructionText>
+          <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad"
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={enteredNumber}
+            onChangeText={numberInputHandler} />
+          <View style={styles.buttonsContainer}>
+            <View style={styles.buttonContainer}>
+              <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+            </View>
+            <View style={styles.buttonContainer}>
+              <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+            </View>
           </View>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -64,6 +66,9 @@ export default StartGameScreen;
 // const deviceHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     // marginTop: deviceHeight < 380 ? 30 : 100,
